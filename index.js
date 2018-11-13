85116135e14ecd2d918577f53c7873ad1ff4fe2e
Wesley Ng <wesjng+learnco@gmail.com>
2018-11-13 05:02:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string)
+  if (string == string.toUpperCase()) {
+    return 'YES INDEED!'
+  }
+  
+  if (string == string.toLowerCase()) {
+    return 'I can\'t hear you!'
+  }
 }
